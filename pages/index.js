8dfaabbd397928a96358ce54d36d1939f996a1c1
shayan-cyber <debroyshayan@gmail.com>
2022-08-08 20:20:36 +0530

    custom-fonts-added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex w-screen h-screen items-center justify-center">
+    <div className="flex w-screen h-screen items-center justify-center font-babaPro">
       New Project
     </div>
   )
