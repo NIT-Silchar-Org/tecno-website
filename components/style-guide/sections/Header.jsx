407f182a5b0c2244d325440092fc08641c7f6075
Nrittik Sarmah <nrittik43@gmail.com>
2022-08-09 16:10:49 +0530

    glow effect added

--- components/style-guide/sections/Header.jsx
+++ components/style-guide/sections/Header.jsx
@@ -5,7 +5,7 @@ import LogoPng from '../../../public/assests/landingPage/tecnoesis.png'
 function Header() {
   return (
     <div className="block">
-      <h1 className="flex justify-center items-center font-roadRage text-4xl">
+      <h1 className="flex justify-center items-center font-roadRage text-4xl drop-shadow-[30px_30px_50px_50px_#FFFFFF]" style={{filter: 'drop-shadow(0px 4px 20px rgba(78, 223, 255, 0.5))'}}>
         Coming Soon
       </h1>
       <div className="mt-10 md:mt-0 relative">
@@ -13,12 +13,12 @@ function Header() {
           <div className="w-[100px] h-[100px] md:w-[250px] md:h-[250px] md:mb-16 relative">
             <Image src={LogoPng} />
           </div>
-          <h1 className="flex justify-center items-center font-lemonMilk text-[3.5rem] md:text-[8.5rem] mt-6 tracking-widest md:-ml-6">
+          <h1 className="flex justify-center items-center font-lemonMilk text-[3.5rem] md:text-[8.5rem] mt-6 tracking-widest md:-ml-6 drop-shadow-[30px_30px_50px_50px_#FFFFFF]" style={{filter: 'drop-shadow(2px 8px 20px rgba(78, 223, 255, 0.5))'}}>
             TECNOESIS
           </h1>
         </div>
         <div className="absolute w-[100vw] -bottom-2 md:bottom-12 flex justify-end">
-          <h1 className="font-roadRage text-xl md:text-3xl w-1/4">2022</h1>
+          <h1 className="font-roadRage text-xl md:text-3xl w-1/4" style={{filter: 'drop-shadow(0px 4px 20px rgba(78, 223, 255, 0.5))'}}>2022</h1>
         </div>
       </div>
 
