7f95f70ccddf9896023f6c91a6b3bba6d286b3d3
Nrittik Sarmah <nrittik43@gmail.com>
2022-09-03 22:44:25 +0530

    modules page template design created

--- components/sections/Sidebar/Sidebar.js
+++ components/sections/Sidebar/Sidebar.js
@@ -1,14 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Sidebar.module.scss'
+import svg1 from '../../../public/assests/modules/Vector14.svg'
+import svg2 from '../../../public/assests/modules/Vector15.svg'
+import Image from 'next/image'
+
+const modules = [
+  'Robotron',
+  'Robotron',
+  'Robotron',
+  'Robotron',
+  'Robotron',
+  'Robotron',
+  'Robotron',
+  'Robotron',
+  'Robotron',
+  'Robotron',
+  'Robotron',
+]
 
 const Sidebar = () => {
+  const [selectedItem, setSelectedItem] = useState(0)
   return (
     <div className={styles.container}>
       <div className={styles.head}>
         <div className={styles.text}>Modules</div>
       </div>
+      <div className={styles.svg_cnt}>
+        <div className={styles.svg}>
+          <Image src={svg1} layout="fill" objectFit="contain" priority="true" />
+        </div>
+      </div>
       <div className={styles.body}>
-        <div className={styles.item}>Robotron</div>
+        <div className={styles.cont}>
+          {modules.map((item, index) => {
+            return (
+              <div className={styles.item_cnt}>
+                <div
+                  className={`${styles.item} ${
+                    selectedItem === index ? styles.selected : ''
+                  }`}
+                  onClick={() => setSelectedItem(index)}
+                  key={index}
+                >
+                  {item}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className={styles.svg_cnt}>
+        <div className={styles.svg2}>
+          <Image src={svg2} layout="fill" objectFit="contain" priority="true" />
+        </div>
       </div>
     </div>
   )
--- pages/modules.js
+++ pages/modules.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Sidebar from '../components/sections/Sidebar/Sidebar'
+import Main from "../components/sections/Modules/Modules"
 
 const Modules = () => {
   return (
-    <div>
-      <Sidebar />
+    <div className="grid grid-cols-7">
+      <div className="col-span-6">
+        <Main />
+      </div>
+      <div>
+        <Sidebar />
+      </div>
     </div>
   )
 }
