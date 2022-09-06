449d63404f5054135a16452ed45f89dde999b869
Nrittik Sarmah <nrittik43@gmail.com>
2022-09-06 09:13:38 +0530

    completed design for modules page

--- components/style-guide/Slider/Slider.js
+++ components/style-guide/Slider/Slider.js
@@ -1,16 +1,18 @@
-import slider from "../../../public/assests/modules/slider.svg"
-import styles from "./Slider.module.scss"
-import Image from "next/image"
+import slider from '../../../public/assests/modules/slider.svg'
+import styles from './Slider.module.scss'
+import Image from 'next/image'
 
-const Slider = ({setActive, active}) =>{
-return(
-    <div className={styles.slider} onClick={()=>{
+const Slider = ({ setActive, active }) => {
+  return (
+    <div
+      className={styles.slider}
+      onClick={() => {
         setActive(true)
-        console.log(active)
-      }}>
-        <Image src={slider} alt=""/>
-      </div>
-)
+      }}
+    >
+      <Image src={slider} alt="" />
+    </div>
+  )
 }
 
-export default Slider;
+export default Slider
