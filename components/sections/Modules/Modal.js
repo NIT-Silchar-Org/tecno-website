8a448e8cba85135e014718d04088652be73a66eb
bikdistinct <bikdev07@gmail.com>
2022-09-10 23:47:38 +0530

    profile page

--- components/sections/Modules/Modal.js
+++ components/sections/Modules/Modal.js
@@ -2,42 +2,36 @@ import React from 'react'
 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IoMdClose } from 'react-icons/io'
+
 function Modal() {
-  return (
-    <div className="modal-wrapper">
-      <div className="modal">
-        {/* <FontAwesomeIcon  icon="fa-xmark"/> */}
+    return (
+        <div className="modal-wrapper">
+            <div className='modal'>
+                <div className='modalImg'></div>
+                <div className='Content-part'>
 
-        <div className="justify-start modalImg"></div>
-        <div className="justify-start">
-          <div className="text-3xl px-2 py-2">
-            <IoMdClose />
-          </div>
-        </div>
+                    <div className="text-3xl cross px-2 py-2">
+                        <IoMdClose />
+                    </div>
 
-        <div className="justify-start">
+                    <div className='desPop'>
+                        <h1 className="text-3xl title">RoboTron</h1>
+                        <div className='modalDes'>
+                            <p className='modalpara'>
+                                Robotronix Engineering Tech. Private Limited is a client-serving company based in Indore.Robotronix Engineering Tech. Private Limited is a client-serving company based in Indore.
+                                Robotronix Engineering Tech. Private Limited is a client-serving company based in Indore.
+                                Robotronix Engineering Tech. Private Limited is a client-serving company based in Indore.
+                            </p>
+                        </div>
+                    </div>
 
-          {/* <div className='modalImg'>
-
-          </div> */}
-          <div className='desPop'>
-            <h1 className="text-3xl title">RoboTron</h1>
-            <div className='modalDes'>
-              <p className='modalpara'>
-                Robotronix Engineering Tech. Private Limited is a client-serving company based in Indore.Robotronix Engineering Tech. Private Limited is a client-serving company based in Indore.
-                Robotronix Engineering Tech. Private Limited is a client-serving company based in Indore.
-                Robotronix Engineering Tech. Private Limited is a client-serving company based in Indore.
-
-              </p>
+                </div>
             </div>
-          </div>
 
 
 
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
-export default Modal
+export default Modal;
