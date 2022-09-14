0be2c23441e833c92ef55f9107ab70e4bf46bf77
bikdistinct <bikdev07@gmail.com>
2022-09-14 16:04:34 +0530

    profile page responsiveness

--- pages/profile.js
+++ pages/profile.js
@@ -16,156 +16,156 @@ function Profile1() {
   return (
     <div>
       <div className="profPhoto2">
-              <Image src={UserIcon} />
-            </div>
-    <div className="backDiv">
-      
-      <div className="profileCard">
-        <div className="profDetail">
+        <Image src={UserIcon} />
+      </div>
+      <div className="backDiv">
+
+        <div className="profileCard">
+          <div className="profDetail">
             <div className='tecnoLogo'>
-            <Image src={Tecnologo} />
+              <Image src={Tecnologo} />
             </div>
-          <div className="profImg">
-          <div className="profPhoto">
-              <Image src={UserIcon} />
-            </div>
-            <div className="eventDetail">
+            <div className="profImg">
+              <div className="profPhoto">
+                <Image src={UserIcon} />
+              </div>
+              <div className="eventDetail">
                 <div className='Attended'>
-                <Image src={Attended} /><br/>
-                <span>Attended<br/></span>
-                <span>6 Events<br/></span>
+                  <Image src={Attended} /><br />
+                  <span>Attended<br /></span>
+                  <span>6 Events<br /></span>
                 </div>
                 <div className='Tecnocoins'>
-                <Image src={TecnoCoin} /><br/>
-                <span>Earned<br/></span>
-                <span>300 Coins<br/></span>
+                  <Image src={TecnoCoin} /><br />
+                  <span>Earned<br /></span>
+                  <span>300 Coins<br /></span>
                 </div>
+              </div>
+            </div>
+            <div className="personalDetail">
+              <ul>
+                <li className='personalDetailIcon'><Image src={Name} />&emsp;Anonymous Dev</li>
+                <li className='personalDetailIcon'><Image src={Institute} />&emsp;National Institute of Technology, Silchar</li>
+                <li className='personalDetailIcon'><Image src={Phone} />&emsp;+91 99999XXXXX</li>
+                <li className='personalDetailIcon'><Image src={Location} />&emsp;Address</li>
+              </ul>
+              <button className='reg'>Registrations</button><br />
+              <button className='logout'>Log Out</button><br />
+
+              201XXXX
             </div>
           </div>
-          <div className="personalDetail">
-            <ul>
-           <li className='personalDetailIcon'><Image src={Name} />&emsp;Anonymous Dev</li>
-           <li className='personalDetailIcon'><Image src={Institute} />&emsp;National Institute of Technology, Silchar</li>
-           <li className='personalDetailIcon'><Image src={Phone} />&emsp;+91 99999XXXXX</li>
-           <li className='personalDetailIcon'><Image src={Location} />&emsp;Address</li>
-          </ul>
-            <button className='reg'>Registrations</button><br/>
-            <button className='logout'>Log Out</button><br />
-            
-            201XXXX
+          <div className="TransactionDetail">
+            <h2 className="transHeading">Transactions</h2>
+            <hr />
+            <br />
+            <ul className='TransactionList'>
+              <li className='EventListLi'>
+                <div className="eventName">
+                  <div>
+                    <Image src={EventIcon} />
+                  </div>
+                  <div>
+                    <span>
+                      ROBOTRON
+                      <br />
+                    </span>
+                    <span>Date</span>
+                  </div>
+                </div>
+                <div className="EarnCoin">
+                  <span>30</span>
+                </div>
+              </li>
+              <li className='EventListLi'>
+                <div className="eventName">
+                  <div>
+                    <Image src={EventIcon} />
+                  </div>
+                  <div>
+                    <span>
+                      ROBOTRON
+                      <br />
+                    </span>
+                    <span>Date</span>
+                  </div>
+                </div>
+                <div className="EarnCoin">
+                  <span>30</span>
+                </div>
+              </li>
+              <li className='EventListLi'>
+                <div className="eventName">
+                  <div>
+                    <Image src={EventIcon} />
+                  </div>
+                  <div>
+                    <span>
+                      ROBOTRON
+                      <br />
+                    </span>
+                    <span>Date</span>
+                  </div>
+                </div>
+                <div className="EarnCoin">
+                  <span>30</span>
+                </div>
+              </li>
+              <li className='EventListLi'>
+                <div className="eventName">
+                  <div>
+                    <Image src={EventIcon} />
+                  </div>
+                  <div>
+                    <span>
+                      ROBOTRON
+                      <br />
+                    </span>
+                    <span>Date</span>
+                  </div>
+                </div>
+                <div className="EarnCoin">
+                  <span>30</span>
+                </div>
+              </li>
+              <li className='EventListLi'>
+                <div className="eventName">
+                  <div>
+                    <Image src={EventIcon} />
+                  </div>
+                  <div>
+                    <span>
+                      ROBOTRON
+                      <br />
+                    </span>
+                    <span>Date</span>
+                  </div>
+                </div>
+                <div className="EarnCoin">
+                  <span>30</span>
+                </div>
+              </li>
+              <li className='EventListLi'>
+                <div className="eventName">
+                  <div>
+                    <Image src={EventIcon} />
+                  </div>
+                  <div>
+                    <span>
+                      ROBOTRON
+                      <br />
+                    </span>
+                    <span>Date</span>
+                  </div>
+                </div>
+                <div className="EarnCoin">
+                  <span>30</span>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="TransactionDetail">
-          <h2 className="transHeading">Transactions</h2>
-          <hr />
-          <br />
-          <ul className='TransactionList'>
-            <li className='EventListLi'>
-              <div className="eventName">
-                <div>
-                  <Image src={EventIcon} />
-                </div>
-                <div>
-                  <span>
-                    ROBOTRON
-                    <br />
-                  </span>
-                  <span>Date</span>
-                </div>
-              </div>
-              <div className="EarnCoin">
-                <span>30</span>
-              </div>
-            </li>
-            <li className='EventListLi'>
-              <div className="eventName">
-                <div>
-                  <Image src={EventIcon} />
-                </div>
-                <div>
-                  <span>
-                    ROBOTRON
-                    <br />
-                  </span>
-                  <span>Date</span>
-                </div>
-              </div>
-              <div className="EarnCoin">
-                <span>30</span>
-              </div>
-            </li>
-            <li className='EventListLi'>
-              <div className="eventName">
-                <div>
-                  <Image src={EventIcon} />
-                </div>
-                <div>
-                  <span>
-                    ROBOTRON
-                    <br />
-                  </span>
-                  <span>Date</span>
-                </div>
-              </div>
-              <div className="EarnCoin">
-                <span>30</span>
-              </div>
-            </li>
-            <li className='EventListLi'>
-              <div className="eventName">
-                <div>
-                  <Image src={EventIcon} />
-                </div>
-                <div>
-                  <span>
-                    ROBOTRON
-                    <br />
-                  </span>
-                  <span>Date</span>
-                </div>
-              </div>
-              <div className="EarnCoin">
-                <span>30</span>
-              </div>
-            </li>
-            <li className='EventListLi'>
-              <div className="eventName">
-                <div>
-                  <Image src={EventIcon} />
-                </div>
-                <div>
-                  <span>
-                    ROBOTRON
-                    <br />
-                  </span>
-                  <span>Date</span>
-                </div>
-              </div>
-              <div className="EarnCoin">
-                <span>30</span>
-              </div>
-            </li>
-            <li className='EventListLi'>
-              <div className="eventName">
-                <div>
-                  <Image src={EventIcon} />
-                </div>
-                <div>
-                  <span>
-                    ROBOTRON
-                    <br />
-                  </span>
-                  <span>Date</span>
-                </div>
-              </div>
-              <div className="EarnCoin">
-                <span>30</span>
-              </div>
-            </li>
-          </ul>
-        </div>
       </div>
-    </div>
     </div>
   )
 }
