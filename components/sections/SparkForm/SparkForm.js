ac1f69543926230a0d786a79c73456743a646c4e
Kavii Suri <kavii@Kaviis-MacBook-Pro.local>
2022-09-18 19:56:34 +0530

    added all things

--- components/sections/SparkForm/SparkForm.js
+++ components/sections/SparkForm/SparkForm.js
@@ -1,6 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileUpload } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 library.add(faFileUpload)
 import styles from '../../../styles/Form.module.scss'
 import { useEffect, useRef, useState } from 'react'
@@ -8,8 +9,6 @@ import { useEffect, useRef, useState } from 'react'
 const UploadControl = ({ children, value, disabled, accept }) => {
   const [fileName, setFileName] = useState()
   const onChange = (e) => {
-    // setFileName(e.target.value)
-    // console.log(e.target.value)
     setFileName(e.target.value)
   }
   return (
@@ -31,21 +30,54 @@ const UploadControl = ({ children, value, disabled, accept }) => {
 }
 export default function SparkForm() {
   const ref = useRef()
+  const [loading, setLoading] = useState(false)
+  const [err, setErr] = useState(false)
   useEffect(() => {
-    console.log('=====')
     setTimeout(() => {
       ref.current?.scrollIntoView({ behaviour: 'smooth' })
     }, 2000)
   }, [])
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    setErr(false)
+    const formData = new FormData(e.target)
+
+    axios
+      .post(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/spark/tshirt`,
+        formData,
+        {
+          headers: {
+            'content-type': 'multipart/form-data',
+          },
+        },
+      )
+      .then((res) => {
+        if (res.data.status < 300) {
+          setLoading(false)
+        } else {
+          setErr(true)
+        }
+      })
+      .catch(() => setErr(true))
+      .finally(() => setLoading(false))
+  }
   return (
-    <form ref={ref} className={styles.form} method="post" action="#">
+    <form
+      onSubmit={onSubmit}
+      ref={ref}
+      className={styles.form}
+      method="post"
+      action="#"
+    >
       <h1>Spark TShirt Form</h1>
       <div className={styles.formFields}>
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name">Name *</label>
         <input type="text" name="name" id="name" required />
       </div>
       <div className={styles.formFields}>
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email *</label>
         <input type="text" name="email" id="email" required />
       </div>
       <div className={styles.formFields}>
@@ -53,12 +85,12 @@ export default function SparkForm() {
         <input type="text" name="scholarId" id="scholarId" />
       </div>
       <div className={styles.formFields}>
-        <label htmlFor="mobileNumber">Mobile Number</label>
+        <label htmlFor="mobileNumber">Mobile Number *</label>
         <input type="text" name="mobileNumber" id="mobileNumber" required />
       </div>
       <div className={styles.formFields}>
-        <label htmlFor="isNITS">Are you from NITS?</label>
-        <select name="isNITS" id="isNITS">
+        <label htmlFor="isNITS">Are you from NITS? *</label>
+        <select name="isNITS" id="isNITS" required>
           <option value="yes">Yes</option>
           <option value="no">No</option>
         </select>
@@ -77,14 +109,14 @@ export default function SparkForm() {
         <div className={styles.btn}>
           <UploadControl accept="image/*">
             <FontAwesomeIcon icon={['fa', 'file-upload']} />
-            Payment Proof
+            Payment Proof*
           </UploadControl>
         </div>
       </div>
 
       <div className={styles.formFields}>
-        <label htmlFor="tshirtSize">Are you from NITS?</label>
-        <select name="tshirtSize" id="tshirtSize">
+        <label htmlFor="tshirtSize">TShirt Size*</label>
+        <select name="tshirtSize" id="tshirtSize" required>
           <option value="XS">XS</option>
           <option value="S">S</option>
           <option value="M">M</option>
@@ -94,12 +126,12 @@ export default function SparkForm() {
         </select>
       </div>
       <div className={styles.formFields}>
-        <label htmlFor="transactionId">Transaction ID</label>
+        <label htmlFor="transactionId">Transaction ID *</label>
         <input type="text" name="transactionId" id="transactionId" required />
       </div>
 
       <button className={styles.btn} type="submit">
-        Submit
+        {loading ? 'Loading...' : err ? 'Error Occured - Retry' : 'Submit'}
       </button>
     </form>
   )
