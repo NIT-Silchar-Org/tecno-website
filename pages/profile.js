4bded7157df7038c60929dbedf5717a03a1566ef
sinharitwick <78261190+sinharitwick@users.noreply.github.com>
2022-09-19 20:39:25 +0530

    resolved conflicts

--- pages/profile.js
+++ pages/profile.js
@@ -106,7 +106,7 @@ function Profile1() {
                 {backendUser?.msg?.registrationId}
               </div>
             </div>
-            <div className="TransactionDetail">
+            {/* <div className="TransactionDetail">
               <h2 className="transHeading">Transactions</h2>
               <hr />
               <br />
@@ -214,38 +214,36 @@ function Profile1() {
                   </div>
                 </li>
               </ul>
+            </div> */}
+            <div className="TransactionDetail">
+              <h2 className="transHeading">Transactions</h2>
+              <hr />
+              {/* <br /> */}
+              <div className="transcomesoon">
+                <h1>Coming soon...</h1>
+              </div>
+              {/* <ul className="TransactionList">
+                <li className="EventListLi">
+                  <div className="eventName">
+                    <div>
+                      <Image src={EventIcon} />
+                    </div>
+                    <div>
+                      <span>
+                        ROBOTRON
+                        <br />
+                      </span>
+                      <span>Date</span>
+                    </div>
+                  </div>
+                  <div className="EarnCoin">
+                    <span>30</span>
+                  </div>
+                </li>
+              </ul> */}
             </div>
           </div>
-<<<<<<< HEAD
-          <div className="TransactionDetail">
-            <h2 className="transHeading">Transactions</h2>
-            <hr />
-            {/* <br /> */}
-            <div className="transcomesoon">
-              <h1>Coming soon...</h1>
-            </div>
-            {/* <ul className="TransactionList">
-              <li className="EventListLi">
-                <div className="eventName">
-                  <div>
-                    <Image src={EventIcon} />
-                  </div>
-                  <div>
-                    <span>
-                      ROBOTRON
-                      <br />
-                    </span>
-                    <span>Date</span>
-                  </div>
-                </div>
-                <div className="EarnCoin">
-                  <span>30</span>
-                </div>
-              </li>
-            </ul> */}
-          </div>
-=======
->>>>>>> 37412e1d45330d5481973a9bafdd9b0049ab418e
+          
         </div>
       </div>
     </>
