355709cb1a7485e4f09ae8c3b55c1bfd98205ead
Nrittik Sarmah <nrittik43@gmail.com>
2022-10-08 11:15:00 +0530

    transactions completed

--- components/style-guide/Transaction/Transaction.js
+++ components/style-guide/Transaction/Transaction.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react'
-import Image from 'next/image'
-import EventIcon from '../../../public/assests/profile/icon.png'
 import { useEffect } from 'react'
 import { fetchEventById } from '../../../utils/events_fetch'
 import styles from './styles.module.scss'
@@ -10,17 +8,20 @@ const Transaction = ({ data }) => {
   const [eventName, setEventName] = useState('')
   const [coinChange, setCoinChange] = useState('')
   const [changeSign, setChangeSign] = useState('')
-  console.log(data)
   useEffect(() => {
-    var event
-    if (data.eventId == null) {
-      setEventName('Event')
-    } else {
-      event = fetchEventById(data.eventId)
-      setEventName(event.msg?.name)
+    if (data.reason === 'ONLINE_EVENT') {
+      setEventName('Online Event')
+    }
+    else if (data.reason != 'REGISTRATION') {
+      setEventName(data.reason.charAt(0) + data.reason.slice(1).toLowerCase())
+    }
+    else {
+      fetchEventById(data.eventId).then((res) => { 
+        setEventName(res.data?.msg?.name)
+      })
     }
 
-    if (data.reason === 'PURCHASE') {
+    if (data.type === 'DEBIT') {
       setCoinChange('Spent')
       setChangeSign('-')
     } else {
@@ -31,23 +32,20 @@ const Transaction = ({ data }) => {
   return (
     <li className={styles.EventListLi}>
       <div className={styles.eventName}>
-        <div style={{marginTop: '5px'}}>
-          <Image src={EventIcon} />
-        </div>
         <div>
           <span>
             {eventName}
             <br />
           </span>
-          <span>{format(new Date(data.createdAt), 'PP')}</span>
+          <span>{format(new Date(data.createdAt), 'PPp')}</span>
         </div>
       </div>
       <div
         className={`${
-          data.reason === 'PURCHASE' ? styles.LoseCoin : styles.EarnCoin
+          data.type === 'DEBIT' ? styles.LoseCoin : styles.EarnCoin
         }`}
       >
-        <span>{`${changeSign} ${data.amount} ${coinChange}`}</span>
+        <span>{`${changeSign}${data.amount} ${coinChange}`}</span>
       </div>
     </li>
   )
